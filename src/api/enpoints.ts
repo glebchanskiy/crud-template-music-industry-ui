88d597f89611d18e909e7ebb10d05e918eb1d5c2
Левков Глеб <levkov.gleb.dev@gmail.com>
2024-05-02 03:00:53 +0300

    dev: change port to 8081

--- src/api/enpoints.ts
+++ src/api/enpoints.ts
@@ -7,7 +7,7 @@ import {
   UpdateEntityById,
 } from "./interfaces";
 
-const host = "http" + "://" + "localhost" + ":8080";
+const host = "http" + "://" + "localhost" + ":8081";
 
 export const api = {
   urlForAllTablesInfo: () => buildUrl(host + "/api/v1/tables")!,
